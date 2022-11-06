bbc1aed8dfdbe4140036183b645be62b4d219607
scotte-snyk <59706011+scotte-snyk@users.noreply.github.com>
2022-11-06 01:28:39 -0400

    Update main.py

--- snyk/scripts/cli/main.py
+++ snyk/scripts/cli/main.py
@@ -1,1 +1,14 @@
-print("Hello from cli!")
+import sys
+import json
+
+print(f"{sys.version=}")
+print(f"{sys.argv=}")
+
+depgraph_file = sys.argv[2]
+
+f = open(depgraph_file) 
+data = json.load(f)
+
+print(json.dumps(data))
+
+#print("Hello from cli!")
